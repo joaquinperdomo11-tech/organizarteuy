0d2f5edf8281b582c222b231738c50b872464c38
joaquinperdomo11-tech <joaquin.perdomo11@gmail.com>
2026-02-27 17:14:35 -0300

    Update sheets.ts

--- lib/sheets.ts
+++ lib/sheets.ts
@@ -433,7 +433,7 @@ function processData(orders: Order[]): DashboardData {
     heatmap,
     skuPerformance,
     currentMonth,
-    prevMonthData,
+    prevMonth: prevMonthData,
     revenueCurrentMonth,
     revenuePrevMonth,
   };
